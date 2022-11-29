573015ce871be60f70f13c001c2d2486e1e49fe9
William Tang <48343678+WiIIiamTang@users.noreply.github.com>
2022-11-28 19:04:54 -0500

    bump to new versioning

--- launcher/src/constants.py
+++ launcher/src/constants.py
@@ -8,7 +8,7 @@ else:
     BASE_PATH = os.path.join(os.getcwd(), "launcher")
 
 # This should be the only place where the lu version is defined
-LAUNCHER_INTERNAL_VERSION = "0.1.0"
+LAUNCHER_INTERNAL_VERSION = "0.1.1"
 
 # get app internal version
 with open(os.path.join(os.path.join(BASE_PATH, ".."), "app", "VERSION"), "r") as f:
